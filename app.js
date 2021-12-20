fbbfba09a2efdf463fa90f5b6089e28a78c537e5
linqiu <1131868300@qq.com>
2021-12-20 19:03:06 +0800

    add corsproxy

---
+++ app.js
@@ -0,0 +1,35 @@
+const axios = require('axios')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3001;
+
+const axatb = axios.create({
+    baseURL:'https://api.alltheblocks.net/atb'
+})
+
+var corsOptions = {
+  origin: function (origin, callback) {
+      if(origin.startsWith('http://localhost')){
+          callback(null, true)
+	  }else if(origin.startsWith('https://wwwa.onrender.com')){
+          callback(null, true)
+      } else {
+          callback(new Error('Not allowed by CORS'))
+      }
+  }
+}
+
+app.use(express.json())
+app.use(cors(corsOptions))
+app.post('/wl', async (req,res)=>{
+    const nf = await axatb.post('/watchlist',req.body)
+    if('data' in nf){
+        res.json(nf.data)
+    }
+})
+
+
+app.listen(port, ()=>{
+    console.log(`started at: http://localhost:${port}`)
+})
